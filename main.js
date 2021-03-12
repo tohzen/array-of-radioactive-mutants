32969c5519fe138e68be640e74d014b1ec6c1caa
tohzen <melvin.l.mobley@gmail.com>
2021-03-12 01:02:20 -0500

    done

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+ function addMeToEnd(arr){
+  arr.push('Colin')
+}
 
+// --------------------------------------------------------------
 
+function addMeToStart(arr){
+  arr.unshift('Colin')
+}
 
+// --------------------------------------------------------------
 
+function changeLast(arr, num){
+  arr[arr.length - 1] = num 
+  // or
+  arr.splice(arr.length - 1, 1, num)
+}
+
+// --------------------------------------------------------------
+
+function changeAllValuesTo(arr, num){
+  for (let i = 0; i < arr.length; i++){
+    arr[i] = num
+  }
+}
+
+// --------------------------------------------------------------
+
+function oddOrEven(arr){
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+      arr[i] = 'even'
+    } else {
+      arr[i] = 'odd'
+    }
+  }
+}
+
+// --------------------------------------------------------------
+
+function changeNextThreeToValue(i, arr, value){
+  for (let n = i; n < i + 3; n++){
+    arr[n] = value
+  }
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
